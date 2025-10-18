f89bc05479177f03e954b2714eb2d926611d0a2d
AkashArman1 <akasharman5678@gmail.com>
2025-10-18 12:49:54 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -53,8 +53,8 @@ Enjoy your stay 🫦
 }
 
 // ✅ Role assignment
-function AssignRole("Dudh Family") {
-    const role = Dudh Family.guild.roles.cache.get("1416494280624705648"); // paste your role ID here
+function AssignRole(member) {
+    const role = member.guild.roles.cache.get("1416494280624705648"); // paste your role ID here
     if (!role) {
         console.log(`Role "${WELCOME_ROLE_NAME}" not found!`);
         return;
